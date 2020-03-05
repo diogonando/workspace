bf51e9f7351fb86c0ecadb419110900628979e8e
Alves Kato <awsdk@88e9fe7b31bb.ant.amazon.com>
2020-03-05 18:11:43 -0300

    first working version

--- workspace.sh
+++ workspace.sh
@@ -5,3 +5,4 @@ brew install neovim
 brew install tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
