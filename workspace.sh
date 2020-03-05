26cb825b5a2b9bf42dc5881d004adb2cb5b01e9b
Alves Kato <awsdk@88e9fe7b31bb.ant.amazon.com>
2020-03-05 18:10:16 -0300

    first working version

---
+++ workspace.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+brew install git
+brew install neovim
+brew install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
