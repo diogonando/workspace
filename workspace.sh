b524aa3e370094da5642fe35daedae561c21c823
Diogo Kato <diogonando@gmail.com>
2020-06-26 20:44:07 -0300

    remove fish

--- workspace.sh
+++ workspace.sh
@@ -5,11 +5,6 @@ brew install tmux
 brew install python3
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "alias python /usr/local/bin/python3" >> ~/.config/fish/config.fish 
-echo "alias pip /usr/local/bin/pip3" >> ~/.config/fish/config.fish 
-echo "set -x PYTHONPATH /usr/local/lib/python3.7/site-packages" >> ~/.config/fish/config.fish
-echo "export LC_ALL=en_US.UTF-8" >> ~/.config/fish/config.fish
-.  ~/.config/fish/config.fish
 pip install jedi
 brew install vim
 
