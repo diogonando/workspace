2da155e04d8d9ae20e7112cfac0d722022e68627
Alves Kato <awsdk@88e9fe7b31bb.ant.amazon.com>
2020-10-20 15:30:04 -0300

    New workspace

--- .config/fish/config.fish
+++
@@ -1,5 +0,0 @@
-alias python /usr/local/bin/python3
-alias pip /usr/local/bin/pip3
-set -x PYTHONPATH /usr/local/lib/python3.7/site-packages
-export LC_ALL=en_US.UTF-8
-export GOPATH=/Users/awsdk/.go 
